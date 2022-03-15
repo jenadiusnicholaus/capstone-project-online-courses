6901fabfcac55add69939e6d19b21da77c07c688
jenadiusnicholaus <ucode888@gmail.com>
2022-03-15 16:01:01 +0300

    Deploy , the mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 
+const logoToggleClass = (element, toggleClass) => {
+    element.classList.toggle(toggleClass);
+  };
+
+document.querySelectorAll('.hamburger-icon').forEach((hamburgIcon) => {
+  hamburgIcon.addEventListener('click', () => {
+    hamburgIcon.classList.toggle('open');
+    logoToggleClass(document.querySelector('.header'), 'solidheader');
+  });
+})
